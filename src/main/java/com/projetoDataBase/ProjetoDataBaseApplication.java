904a14312d2284ca106be6ab38788b87e31a4400
karol castro <karolinycosta@yahoo.com.br>
2021-09-20 09:45:47 -0300

    service

--- src/main/java/com/projetoDataBase/ProjetoDataBaseApplication.java
+++ src/main/java/com/projetoDataBase/ProjetoDataBaseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjetoDataBaseApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProjetoDataBaseApplication.class, args);
 	}
 
