5762dfb7f166ff4428f9e4c1f96f7bd248475d56
knigosky <karolinycosta@yahoo.com.br>
2021-09-19 21:07:51 -0300

    iniciando

---
+++ src/main/java/com/projetoDataBase/ProjetoDataBaseApplication.java
@@ -0,0 +1,13 @@
+package com.projetoDataBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDataBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDataBaseApplication.class, args);
+	}
+
+}
