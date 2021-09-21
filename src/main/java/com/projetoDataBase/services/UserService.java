2c581a4053c1558c3a6dbe0796eebea912e62a2f
karol castro <karolinycosta@yahoo.com.br>
2021-09-21 08:51:50 -0300

    EntityNotFoundException

--- src/main/java/com/projetoDataBase/services/UserService.java
+++ src/main/java/com/projetoDataBase/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -44,12 +45,17 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = userRepository.getOne(id);
-        updateDta(entity, obj);
+        try {
+            User entity = userRepository.getOne(id);
+            updateData(entity, obj);
             return userRepository.save(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
-    private void updateDta(User entity, User obj) {
+    private void updateData(User entity, User obj) {
         entity.setName(obj.getName());
         entity.setEmail(obj.getEmail());
         entity.setPhone(obj.getPhone());
