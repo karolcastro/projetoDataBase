803ad741e2fef673bd6e4a8ba1aded845e524cab
karol castro <karolinycosta@yahoo.com.br>
2021-09-21 08:45:27 -0300

    DatabaseException and delete

--- src/main/java/com/projetoDataBase/resources_controller/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/projetoDataBase/resources_controller/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.projetoDataBase.resources_controller.exceptions;
 
+import com.projetoDataBase.services.exceptions.DatabaseException;
 import com.projetoDataBase.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,4 +20,13 @@ public class ResourceExceptionHandler {
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> dataBase(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
 }
